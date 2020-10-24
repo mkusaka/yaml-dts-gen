1cced2ca38e2a912dfd8fa1f743b076270f06e1d
mkusaka <hinoshita1992@gmail.com>
2020-10-24 22:31:39 +0900

    feat: emit declartion file from ts compiler api

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Its filename follows original yaml file, like original.yml, then generate origin
 ```bash
 npx yaml-dts-gen [target yaml file]
 ```
+
+# example
+
+```bash
+curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore.yaml -o petstore.yml
+npx yaml-dts-gen petstore.yml
+```
--- cli.ts
+++ cli.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as yaml from "js-yaml";
+import { Project } from "ts-morph";
 
 function gen(filename: string) {
   const content = fs.readFileSync(filename, { encoding: "utf-8" });
@@ -10,12 +11,19 @@ function gen(filename: string) {
     2
   )} as const;`;
 
-  console.log(defaultExportedJson);
-
-  fs.writeFileSync(
-    process.argv[2].replace(".yml", ".ts"),
-    defaultExportedJson
+  const project = new Project({
+    compilerOptions: {
+      outDir: "./",
+      declaration: true,
+      emitDeclarationOnly: true,
+    },
+  });
+  project.createSourceFile(
+    filename.replace(".yml", ".ts"),
+    defaultExportedJson,
+    { overwrite: true }
   );
+  project.emitSync();
 }
 
 export const run = async () => {
